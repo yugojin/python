da937b56f6b8f08cb3b172de67ce411e8861fc65
yugojin <yugojinu5@gmail.com>
2021-02-07 17:18:24 +0900

    数値

--- test03.py
+++ test03.py
@@ -1,5 +1,7 @@
-test_str =  """
-test_1
-test_2
-"""
-print(test_str)
+
+test_integer = 100
+ 
+print(test_integer + 10)    # 加算（足し算）
+print(test_integer - 10)    # 減算（引き算）
+print(test_integer * 10)    # 乗算（掛け算）
+print(test_integer / 10)    # 除算（割り算）
