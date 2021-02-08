07a6d27c2de55c9b27b235e8d4a9a5f441c93fbd
yugojin <yugojinu5@gmail.com>
2021-02-08 10:00:50 +0900

    日付、時間

--- test01.py
+++ test01.py
@@ -1,1 +1,28 @@
-python
+import datetime
+ 
+ 
+today = datetime.date.today()
+todaydetail = datetime.datetime.today()
+ 
+# 今日の日付
+print('----------------------------------')
+print(today)
+print(todaydetail)
+ 
+# 今日に日付：それぞれの値
+print('----------------------------------')
+print(today.year)
+print(today.month)
+print(today.day)
+print(todaydetail.year)
+print(todaydetail.month)
+print(todaydetail.day)
+print(todaydetail.hour)
+print(todaydetail.minute)
+print(todaydetail.second)
+print(todaydetail.microsecond)
+ 
+# 日付のフォーマット
+print('----------------------------------')
+print(today.isoformat())
+print(todaydetail.strftime("%Y/%m/%d %H:%M:%S"))
