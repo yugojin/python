f5d0067d231b09d4247f3fb07cfd98a17dadc4c0
yugojin <yugojinu5@gmail.com>
2021-02-10 16:59:55 +0900

    リスト

--- test03.py
+++ test03.py
@@ -1,17 +1,7 @@
-import datetime
+test_list_1 = ['python', '-', 'izm', '.', 'com']
+print(test_list_1)
  
+print('--------------------------------')
  
-def get_today():
- 
-    today = datetime.datetime.today()
-    value = (today.year, today.month, today.day)
- 
-    return value
- 
- 
-test_tuple = get_today()
- 
-print(test_tuple)
-print(test_tuple[0])
-print(test_tuple[1])
-print(test_tuple[2])
+for i in test_list_1:
+    print(i)
