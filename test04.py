dab31f7e2e923d85dec14c51689676eb0558843f
yugojin <yugojinu5@gmail.com>
2021-02-11 10:11:22 +0900

    ディクショナリ

---
+++ test04.py
@@ -0,0 +1,10 @@
+test_dict_1 = {'YEAR':'2010', 'MONTH':'1', 'DAY':'20'}
+ 
+print(test_dict_1)
+ 
+print('=================================')
+ 
+for i in test_dict_1:
+    print(i)
+    print(test_dict_1[i])
+    print('--------------------------------')
