afc6f4cb761cf1d41c9beb887cc9058e2e046274
yugojin <yugojinu5@gmail.com>
2021-02-18 11:24:30 +0900

    if文

--- test01.py
+++ test01.py
@@ -1,8 +1,10 @@
-import os
+value = 2
  
- 
-PROJECT_DIR = 'C:¥python-izm'
-SETTINGS_FILE = 'settings.ini'
- 
-print(os.path.join(PROJECT_DIR, SETTINGS_FILE))
-print(os.path.join(PROJECT_DIR, 'settings_dir', SETTINGS_FILE))
+if value == 1:
+    print('valueの値は1です')
+elif value == 2:
+    print('valueの値は2です')
+elif value == 3:
+    print('valueの値は3です')
+else:
+    print('該当する値はありません')
