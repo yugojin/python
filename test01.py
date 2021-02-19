0b0bda1d0cd11d45f2d022c93cca1fa855663eb4
yugojin <yugojinu5@gmail.com>
2021-02-19 19:31:59 +0900

    for文

--- test01.py
+++ test01.py
@@ -1,10 +1,10 @@
-value = 2
+for_sample = []
+for_sample.append("python")
+for_sample.append("-")
+for_sample.append("izm")
+for_sample.append("for")
+for_sample.append("statement")
+for_sample.append("sample")
  
-if value == 1:
-    print('valueの値は1です')
-elif value == 2:
-    print('valueの値は2です')
-elif value == 3:
-    print('valueの値は3です')
-else:
-    print('該当する値はありません')
+for value in for_sample:
+    print(value)
