7bbccd2e62d6490cec6d85618658988b0482c097
yugojin <yugojinu5@gmail.com>
2021-02-20 10:19:49 +0900

    while文

--- test01.py
+++ test01.py
@@ -1,10 +1,5 @@
-for_sample = []
-for_sample.append("python")
-for_sample.append("-")
-for_sample.append("izm")
-for_sample.append("for")
-for_sample.append("statement")
-for_sample.append("sample")
+counter = 0
  
-for value in for_sample:
-    print(value)
+while counter < 10:
+    counter += 1
+    print(counter)
