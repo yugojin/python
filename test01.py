71f3345849dd0f1a559c53a653345cb409dfe027
yugojin <yugojinu5@gmail.com>
2021-03-02 15:17:41 +0900

    関数

--- test01.py
+++ test01.py
@@ -1,18 +1,4 @@
-def exception_test(value_1, value_2):
+def test_func():
+    print('call test_func')
  
-    print('＝＝＝＝計算開始＝＝＝＝')
- 
-    result = 0
- 
-    try:
-        result = value_1 + value_2
-    except:
-        print('計算出来ませんでした！')
-    finally:
-        print('計算終了')
- 
-    return result
-     
- 
-print(exception_test(100, 200))
-print(exception_test(100, '200'))
+test_func()
