6a0700d573c4fba5efa5388a7a65b5e5a841b149
yugojin <yugojinu5@gmail.com>
2021-03-04 09:57:03 +0900

    関数

--- test01.py
+++ test01.py
@@ -1,4 +1,23 @@
-def test_func():
-    print('call test_func')
+def test_func(num_1, num_2, oprn):
  
-test_func()
+    if oprn == 1:
+        print('足し算開始')
+        print(num_1 + num_2)
+ 
+    elif oprn == 2:
+        print('引き算開始')
+        print(num_1 - num_2)
+ 
+    elif oprn == 3:
+        print('掛け算開始')
+        print(num_1 * num_2)
+ 
+    elif oprn == 4:
+        print('割り算開始')
+        print(num_1 / num_2)
+ 
+    else:
+        print('不明なオペレーション指定です')
+         
+ 
+test_func(100, 10, 3)
