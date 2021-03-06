25d30b250727255f790c5f4d79fc9d11802ecfa7
yugojin <yugojinu5@gmail.com>
2021-03-06 21:31:11 +0900

    可変長引数

--- test01.py
+++ test01.py
@@ -1,22 +1,4 @@
-def test_func(num_1, num_2, oprn=1):
+def test_func(*args):
+    print(args)
  
-    if oprn == 1:
-        print('足し算開始')
-        print(num_1 + num_2)
- 
-    elif oprn == 2:
-        print('引き算開始')
-        print(num_1 - num_2)
- 
-    elif oprn == 3:
-        print('掛け算開始')
-        print(num_1 * num_2)
- 
-    elif oprn == 4:
-        print('割り算開始')
-        print(num_1 / num_2)
- 
-    else:
-        print('不明なオペレーション指定です')
- 
-test_func(100, 10)
+test_func(1, 2, 3, 4, 5)
