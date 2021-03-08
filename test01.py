2c89030443c6f64c30bbe5ac03e045625d82d622
yugojin <yugojinu5@gmail.com>
2021-03-08 11:53:16 +0900

    可変長引数

--- test01.py
+++ test01.py
@@ -1,5 +1,10 @@
-def test_func(code, name, *args):
-    print(code, name)
+def test_func(code, name, kana, *args, **kwargs):
+    print(code, name, kana)
     print(args)
+    print(kwargs)
  
-test_func(100, 'python-izm', 'JP', 'US')
+test_func(
+    100, 'python-izm', u'パイソンイズム',
+    'JP', 'US', 
+    email='xxxx', city='Tokyo'
+)
