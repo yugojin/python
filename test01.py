c6d2665febd23f74053d364952010e8fa89c0e65
yugojin <yugojinu5@gmail.com>
2021-03-10 12:42:49 +0900

    クラス継承

--- test01.py
+++ test01.py
@@ -1,15 +1,21 @@
-class TestClass:
+class Country:
  
-    def __init__(self, code, name):
-        self.code = code
-        self.name = name
+    def __init__(self, country_name):
+        self.country_name = country_name
+ 
+ 
+class City(Country):
+ 
+    def __init__(self, country_name, city_name):
+        super().__init__(country_name)
+        self.city_name = city_name
  
  
 classes = []
-classes.append(TestClass(1, 'テスト１'))
-classes.append(TestClass(2, 'テスト２'))
+classes.append(City('Japan', 'Tokyo'))
+classes.append(City('USA', 'Washington, D.C.'))
  
 for test_cls in classes:
     print('===== Class =====')
-    print('code --> ' + str(test_cls.code))
-    print('name --> ' + test_cls.name)
+    print('country_name --> ' + test_cls.country_name)
+    print('city_name --> ' + test_cls.city_name)
