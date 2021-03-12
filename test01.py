4f7919a6182c0d0ba8cd128efae142805216c73b
yugojin <yugojinu5@gmail.com>
2021-03-12 15:49:21 +0900

    新クラススタイル

--- test01.py
+++ test01.py
@@ -1,6 +1,19 @@
 # 新スタイルクラス
-class NewStyleClass:
-    pass
+class NewStyleClassBase(object):
+ 
+    def test_method(self, msg):
+        print('NewStyleClassBase: {}'.format(msg))
  
  
-print(type(NewStyleClass))
+# 新スタイルのクラスを継承
+class NewStyleClass(NewStyleClassBase):
+ 
+    def test_method(self, msg):
+        print('NewStyleClass: {}'.format(msg))
+        super().test_method(msg)
+        # super(NewStyleClass, self).test_method(msg)
+        # NewStyleClassBase.test_method(self, msg)
+ 
+ 
+new = NewStyleClass()
+new.test_method('method call')
