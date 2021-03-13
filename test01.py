cbcf0279a91f28029b52f82acc52b9fdeac33513
yugojin <yugojinu5@gmail.com>
2021-03-13 10:57:07 +0900

    メソッド一覧

--- test01.py
+++ test01.py
@@ -1,19 +1,16 @@
 # 新スタイルクラス
-class NewStyleClassBase(object):
+class TestClass:
  
-    def test_method(self, msg):
-        print('NewStyleClassBase: {}'.format(msg))
+    # インスタンスメソッド
+    def sample_instancemethod(self, arg_1):
+        pass
  
+    # クラスメソッド
+    @classmethod
+    def sample_classmethod(cls, arg_1):
+        pass
  
-# 新スタイルのクラスを継承
-class NewStyleClass(NewStyleClassBase):
- 
-    def test_method(self, msg):
-        print('NewStyleClass: {}'.format(msg))
-        super().test_method(msg)
-        # super(NewStyleClass, self).test_method(msg)
-        # NewStyleClassBase.test_method(self, msg)
- 
- 
-new = NewStyleClass()
-new.test_method('method call')
+    # スタティックメソッド
+    @staticmethod
+    def sample_staticmethod(arg_1, arg_2):
+        pass
