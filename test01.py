fb19ef1e2cd013a3c470fb3acec7e5edb5a21d06
yugojin <yugojinu5@gmail.com>
2021-03-18 07:50:48 +0900

    クラスメソッド

--- test01.py
+++ test01.py
@@ -1,14 +1,29 @@
+import datetime
+ 
+ 
 class TestClass:
  
-    # スタティックメソッド
-    @staticmethod
-    def sample_staticmethod(x, y):
-        return x + y
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+ 
+    # クラスメソッド
+    @classmethod
+    def sample_classmethod(cls, date_diff=0):
+        today = datetime.date.today()
+        d = today + datetime.timedelta(days=date_diff)
+        return cls(d.year, d.month, d.day)
  
  
 # インスタンス化しないで呼び出し
-print(TestClass.sample_staticmethod(10, 100))
+test_class_1 = TestClass.sample_classmethod()
+print(test_class_1.year, test_class_1.month, test_class_1.day)
  
-# インスタンス化してからも呼び出せる
-test_class = TestClass()
-print(test_class.sample_staticmethod(100, 1000))
+# インスタンス化しないで呼び出し
+test_class_2 = TestClass.sample_classmethod(-10)
+print(test_class_2.year, test_class_2.month, test_class_2.day)
+ 
+# 通常のインスタンス
+test_class_3 = TestClass(2000, 1, 1)
+print(test_class_3.year, test_class_3.month, test_class_3.day)
