447becf4650b3a051237fcf1f9b02e4fe6684ec9
yugojin <yugojinu5@gmail.com>
2021-03-21 15:14:12 +0900

    ジェネレーター

--- test01.py
+++ test01.py
@@ -1,8 +1,7 @@
-def plus_value(num_1, num_2):
-    return num_1 + num_2
+# ループ開始前に 1 から 10 まですべてを確保
+for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    print(i)
  
- 
-print(plus_value(10, 100))
- 
-l_func = lambda num_1, num_2: num_1 + num_2
-print(l_func(10, 100))
+# 呼び出されるごとに値を生成
+for i in <ジェネレータ>:
+    print(i)
