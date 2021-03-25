7134b7683ff8e7b7b041ef78d3cd8c01dd134f92
yugojin <yugojinu5@gmail.com>
2021-03-25 16:41:07 +0900

    with文

--- test01.py
+++ test01.py
@@ -1,7 +1,9 @@
-# ループ開始前に 1 から 10 まですべてを確保
-for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
-    print(i)
+with open('file.txt') as f:
+     
+    #
+    # 何らかの処理を行う
+    #
+     
+    print(f.closed)
  
-# 呼び出されるごとに値を生成
-for i in <ジェネレータ>:
-    print(i)
+print(f.closed)
